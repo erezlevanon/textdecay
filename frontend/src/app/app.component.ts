5196b13fd347ba113278f8fd48118a163ac193b3
erezlevanon <erez.levanon@mail.huji.ac.il>
2024-08-12 00:25:22 +0200

    minimum decay factor is now zero. check sensor 10 times a second.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -27,7 +27,7 @@ import {AsyncPipe, NgForOf, NgIf} from "@angular/common";
 export class AppComponent implements OnInit {
   title = 'decaying_storage_frontend';
 
-  private readonly interval = interval(200);
+  private readonly interval = interval(100);
   readonly latestRead = this.interval.pipe(
     switchMap(() => this.sensorApi.getSensorRead()),
     shareReplay(),
