9368250e780812952c13acaf879d356bed849b45
erezlevanon <erez.levanon@mail.huji.ac.il>
2024-08-13 13:43:51 +0200

    extract some constant, fix mapvalue function in text.service.ts

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -17,12 +17,15 @@ enum Directions {
   DECAY = -1
 }
 
+// Playable constants
 const DIRECTION: Directions = Directions.APPEAR;
-
 const DECAY_RATE = 0.01;
 const INITIAL_RESPONSE_TIME_SECOND = 3;
 const SENSOR_READ_TIME = 100;
 const SIGNAL_TO_NOISE = 0.8;
+const FANCY_HIDDEN = false;
+
+// Calculated constants
 const READS_TO_START = INITIAL_RESPONSE_TIME_SECOND * 1000 / SENSOR_READ_TIME;
 const ALLOWED_MIN = TextService.NORMALIZED_MIN * (1 - DECAY_RATE) ** READS_TO_START;
 const ALLOWED_MAX = TextService.NORMALIZED_MAX * (1 + DECAY_RATE) ** READS_TO_START;
@@ -51,7 +54,6 @@ export class AppComponent implements OnInit {
       const change = (dir * noise * DECAY_RATE);
       const directionalChange = DIRECTION === Directions.APPEAR ? 1 + change : 1 - change;
       this.decayFactor = Math.min(Math.max(this.decayFactor * directionalChange, ALLOWED_MIN), ALLOWED_MAX);
-      console.log(this.decayFactor);
     }),
     tap(() => this.updateClasses()));
 
@@ -85,11 +87,12 @@ export class AppComponent implements OnInit {
     for (const term of this.text.terms.value) {
       try {
         for (const e of this.elem.nativeElement.querySelectorAll(`.${term}`)) {
-          // console.log(read, normalizedScore);
           timer(Math.random() * 2000).subscribe(() => {
             if (this.text.getTFIDF(term) > this.decayFactor) {
               e.classList.add('hidden');
-              // e.classList.add(`hidden-${(term.length + i) % 10}`);
+              if (FANCY_HIDDEN) {
+                e.classList.add(`hidden-${(term.length + i) % 10}`);
+              }
             } else {
               e.classList.remove('hidden');
             }
