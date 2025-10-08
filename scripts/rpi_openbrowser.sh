6bdffb327f6eeb737c6d2f090a64eeb1f0820bb3
erezlevanon <erezslp@gmail.com>
2025-10-08 19:38:32 +0200

    updated for newest rpi os (trixie 64-bit)

--- scripts/rpi_openbrowser.sh
+++ scripts/rpi_openbrowser.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
+# wait for server to load
+sleep 5s
+
 xhost +local:
 export DISPLAY=:0
+
+export XAUTHORITY=/home/yotamerez/.Xauthority
+
+# For PulseAudio
+export XDG_RUNTIME_DIR=/run/user/1000
 
 # Disable xset blanking, let xscreensaver handle that.
 xset s noblank
@@ -12,8 +20,15 @@ xset s off
 # Hide the mouse cursor.
 # unclutter -idle 1 -root &
 
+# just in case.
+sleep 1s
+
 # Start Chromium.
-chromium-browser --kiosk --noerrdialogs --disable-infobars --autoplay-policy=no-user-gesture 'http://127.0.0.1:8000'
+# old dwd
+# chromium-browser --kiosk --noerrdialogs --disable-infobars --autoplay-policy=no-user-gestu>
+# new dwd
+chromium --kiosk --autoplay-policy=no-user-gesture-required --noerrdialogs --disable-infobars 'http://127.0.0.1:8000'
+
 # chromium-browser --noerrdialogs --autoplay-policy=no-user-gesture 'http://127.0.0.1:8000'
 # chromium-browser 'http://127.0.0.1:8000'
 
